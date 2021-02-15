2ba7d5645a00b0b2e0c9ef3b6b15c6a8f7bf2f74
Quentin Vansteene <vansteene.quentin@gmail.com>
2021-02-15 15:00:49 +0100

    0.1.6 - add rally features and config options. Improves sending fleet procedure

--- OGM.js
+++ OGM.js
@@ -9,7 +9,7 @@
 // @grant		   GM_xmlhttpRequest
 // @updateURL      https://raw.githubusercontent.com/SeptimusVII/OGM/main/OGM.js
 // @downloadURL    https://raw.githubusercontent.com/SeptimusVII/OGM/main/OGM.js
-// @version        0.1.5
+// @version        0.1.6
 
 // @include        *.ogame*gameforge.com/game/index.php?page=*
 // @exclude        *.ogame*gameforge.com/game/index.php?page=displayMessageNewPage*
